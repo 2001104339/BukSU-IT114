152f23f76eceab0c96864c03a5b068dedb3a419b
2001104339 <73920380+2001104339@users.noreply.github.com>
2021-04-13 09:35:09 +0800

    Add files via upload

---
+++ 2darrays.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x[3][2];
+    int id, pin;
+    x[0][0]=2255;
+    x[0][1]=1234;
+    x[1][0]=2256;
+    x[1][1]=4321;
+    x[2][0]=2257;
+    x[2][1]=1243;
+
+    printf("ID Number: ");
+    scanf("%d", &id);
+    printf("PIN CODE: ");
+    scanf("%d", &pin);
+
+    if(id==x[0][0], pin==x[0][1])
+    {
+    printf("You have successfully logged in\nID#:%d", id);
+    }else if(id==x[1][0], pin==x[1][1])
+    {
+    printf("You have successfully logged in\nID#:%d", id);
+    }else if(id==x[2][0], pin==x[2][1])
+    {
+    printf("You have successfully logged\nID#:%d", id);
+    }else
+    {
+    printf("Invalid ID/PIN!");
+    }
+
+    return 0;
+}
