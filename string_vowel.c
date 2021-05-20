2c3db4f268ae36ad22ca01e32bd921d86aa341d4
2001104339 <73920380+2001104339@users.noreply.github.com>
2021-05-20 18:10:03 +0800

    Add files via upload

---
+++ string_vowel.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char s[100];
+    int i,vowels=0, consonants=0, l;
+
+    printf("Input a sentence: ");
+    gets(s);
+
+    l = strlen(s);
+
+    i=0;
+    while(s[i]!='\0')
+        {
+        if(s[i]=='a' ||s[i]=='e' ||s[i]=='i' ||s[i]=='o' ||s[i]=='u')
+            s[i]=s[i]-32;
+        i++;
+        }
+
+    for(i=0;s[i];i++)
+    {
+    	if((s[i]>=65 && s[i]<=90)|| (s[i]>=97 && s[i]<=122))
+    	{
+
+            if(s[i]=='a'|| s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O' ||s[i]=='U')
+		      vowels++;
+            else
+             consonants++;
+        }
+
+ 	}
+
+    printf("String Converted: ");
+    puts(s);
+    printf("\nString length: %d\n", l);
+    printf("vowels = %d\n",vowels);
+    printf("consonants = %d\n",consonants);
+
+    return 0;
+}
